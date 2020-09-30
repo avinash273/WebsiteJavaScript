e73946cbb300a4374c558bae7f69121ba50970ad
avinash273 <avinash.shanker@mavs.uta.edu>
2020-09-30 15:50:13 -0500

    objects and constructors

---
+++ objects/script.js
@@ -0,0 +1,20 @@
+function Course(title, instructor, level, views){
+  this.title = title;
+  this.instructor = instructor;
+  this.level = level;
+  this.views = views;
+  this.updateViews = function(){
+    return this.views++;
+  }
+}
+
+var course01 = new Course("Java", "Joe", 1, 10);
+var course02 = new Course("JavaScript", "Frank", 2, 56);
+
+var courses = [course01, course02];
+
+course01.updateViews();
+courses[0].updateViews()
+
+console.log(courses);
+console.log(courses[0].views);
